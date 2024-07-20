ca7c9b5a1e704224980f48f0d0a3e1697c78159d
skylatian <53362705+skylatian@users.noreply.github.com>
2024-07-19 21:32:56 -0400

    Initial Commit

---
+++ threebuttons/threebuttonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  threebuttonsApp.swift
+//  threebuttons
+//
+//  Created by skylatian on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct threebuttonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
