86a0a89ab91efb46a67c3f9124153a2c596e94ee
skylatian <53362705+skylatian@users.noreply.github.com>
2024-07-19 23:16:22 -0400

    working top quarter touches+colored dot

---
+++ threebuttons/TouchDataModel.swift
@@ -0,0 +1,22 @@
+//
+//  TouchDataModel.swift
+//  threebuttons
+//
+//
+//  Created by skylatian on 7/19/24.
+//
+
+import Foundation
+import AppKit
+
+import Foundation
+
+class TouchDataModel {
+    static let shared = TouchDataModel()
+    var topQuarterTouchesActive = false {
+        didSet {
+            // You could trigger additional actions here if needed when the value changes.
+            print("Top Quarter Touches Active: \(topQuarterTouchesActive)")
+        }
+    }
+}
